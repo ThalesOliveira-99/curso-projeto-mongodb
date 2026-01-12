5bfe98bfb70941aab231b14bbe42b3284d597f7d
Thales Oliveira <thales930@gmail.com>
2026-01-12 13:12:07 -0300

    Obtendo um post por id

---
+++ src/main/java/com/thalesoliveira/workshopmongo/resources/PostResource.java
@@ -0,0 +1,53 @@
+package com.thalesoliveira.workshopmongo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thalesoliveira.workshopmongo.domain.Post;
+import com.thalesoliveira.workshopmongo.services.PostService;
+
+//Indica que esta classe é um recurso web REST (vai responder com dados JSON, e não páginas HTML)
+@RestController
+//Define o caminho (endpoint) base da URL para acessar este recurso (ex: localhost:8080/posts)
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	// Injeta a dependência do Serviço (PostService), permitindo que o Controlador
+	// delegue para ele a execução das regras de negócio
+	@Autowired
+	private PostService service;
+
+	// Mapeia requisições do tipo GET que trazem um id na URL (ex: /users/123). As
+	// chaves { } indicam que essa parte do caminho é variável.
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	// O @PathVariable pega o valor do "{id}" que veio na URL e o atribui à variável
+	// 'id' para ser usada na busca. O retorno será um UserDTO envelopado.
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		// Aciona a camada de serviço para buscar o usuário pelo ID e guarda o objeto
+		// original (Entidade) retornado na variável 'obj'
+		Post obj = service.findById(id);
+		// Converte o objeto 'obj' (Entidade) para 'UserDTO' ali mesmo e o envia no
+		// corpo da resposta com status 200 (OK), garantindo que apenas os dados
+		// filtrados sejam expostos
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	// Mapeia requisições GET para a raiz do endpoint (/posts). 
+	// Como não tem "/{id}", ele atende quando chamamos apenas localhost:8080/posts
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAll() {
+	    
+	    // Chama o serviço para buscar a lista
+	    List<Post> list = service.findAll();
+	    
+	    // Retorna a lista no corpo da resposta com status 200 OK
+	    return ResponseEntity.ok().body(list);
+	}
+
+}
---
+++ src/main/java/com/thalesoliveira/workshopmongo/services/PostService.java
@@ -0,0 +1,42 @@
+package com.thalesoliveira.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thalesoliveira.workshopmongo.domain.Post;
+import com.thalesoliveira.workshopmongo.repository.PostRepository;
+import com.thalesoliveira.workshopmongo.services.exception.ObjetoNotFoundException;
+
+// Indica que esta classe é um serviço do Spring, responsável por conter a lógica de negócio 
+//(regras da aplicação) antes de salvar no banco
+@Service
+public class PostService {
+
+	// Realiza a injeção de dependência automática: o Spring instancia e conecta o
+	// objeto UserRepository aqui para você (sem precisar dar "new")
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		// Chama o repositório para buscar pelo ID. O retorno é 'Optional' porque o
+		// resultado é incerto:
+		// funciona como uma "caixa" que pode conter o usuário ou estar vazia (null
+		// safe).
+		Optional<Post> obj = repo.findById(id);
+		// Tenta abrir a "caixa":
+		// 1. Se tiver um usuário dentro, retorna o objeto User.
+		// 2. Se a caixa estiver vazia (orElseThrow), dispara a sua exceção
+		// personalizada instantaneamente.
+		return obj.orElseThrow(() -> new ObjetoNotFoundException("Objeto não encontrado"));
+	}
+
+	// Método responsável por buscar TODOS os posts existentes no banco de dados
+	public List<Post> findAll() {
+		// O repo.findAll() é um método padrão do MongoRepository que retorna uma lista
+		// completa
+		return repo.findAll();
+	}
+}
